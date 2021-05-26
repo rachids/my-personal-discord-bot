782f1e16512c1a86b5162e25f4377b324c87f291
rachids <livecoding@rachids.ca>
2021-05-25 21:03:02 -0400

    Added Teleport API service and command to have population number.

---
+++ src/services/teleport.ts
@@ -0,0 +1,25 @@
+const baseURL = 'https://api.teleport.org/api/';
+
+export async function getInfosForCity(city: string)
+{
+    let geoNameURL = await getGeoname(city);
+    let cityInfos = await getCityInfo(geoNameURL);
+
+    return cityInfos;
+}
+
+async function getGeoname(city: string) {
+    let url = baseURL + 'cities/?search=' + city;
+
+    let response = await fetch(url);
+    let body = await response.json();
+    return body['_embedded']['city:search-results'][0]['_links']['city:item']['href'];
+}
+
+async function getCityInfo(url: string)
+{
+    let response = await fetch(url);
+    let body = await response.json();
+
+    return body;
+}
