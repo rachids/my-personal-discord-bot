bf77983416d7901c799e564cb7084ed245e7e660
rachids <rachids@users.noreply.github.com>
2021-05-26 01:03:32 +0000

    change: prettier code

--- src/commands/city.ts
+++ src/commands/city.ts
@@ -4,17 +4,20 @@ import { createCommand } from "../utils/helpers.ts";
 createCommand({
   name: `city`,
   description: "Donnes des infos sur la première ville qui correspond.",
-  arguments: [{
-    name: "city",
-    type: "string",
-    missing: (message) => {
-      message.reply(":thinking: Il manquerait pas le nom de la ville par hasard ?")
-    }
-  }],
+  arguments: [
+    {
+      name: "city",
+      type: "string",
+      missing: (message) => {
+        message.reply(":thinking: Il manquerait pas le nom de la ville par hasard ?");
+      },
+    },
+  ],
   execute: async function (message, args) {
-
     let infos = await getInfosForCity(args.city);
 
-    message.reply(`Tu savais que *` + infos.name + `* compte pas moins de *` + infos.population +`* habitants ?! La dinguerie.`);
+    message.reply(
+      `Tu savais que *` + infos.name + `* compte pas moins de *` + infos.population + `* habitants ?! La dinguerie.`
+    );
   },
 });
--- src/services/teleport.ts
+++ src/services/teleport.ts
@@ -1,25 +1,23 @@
-const baseURL = 'https://api.teleport.org/api/';
+const baseURL = "https://api.teleport.org/api/";
 
-export async function getInfosForCity(city: string)
-{
-    let geoNameURL = await getGeoname(city);
-    let cityInfos = await getCityInfo(geoNameURL);
+export async function getInfosForCity(city: string) {
+  let geoNameURL = await getGeoname(city);
+  let cityInfos = await getCityInfo(geoNameURL);
 
-    return cityInfos;
+  return cityInfos;
 }
 
 async function getGeoname(city: string) {
-    let url = baseURL + 'cities/?search=' + city;
+  let url = baseURL + "cities/?search=" + city;
 
-    let response = await fetch(url);
-    let body = await response.json();
-    return body['_embedded']['city:search-results'][0]['_links']['city:item']['href'];
+  let response = await fetch(url);
+  let body = await response.json();
+  return body["_embedded"]["city:search-results"][0]["_links"]["city:item"]["href"];
 }
 
-async function getCityInfo(url: string)
-{
-    let response = await fetch(url);
-    let body = await response.json();
+async function getCityInfo(url: string) {
+  let response = await fetch(url);
+  let body = await response.json();
 
-    return body;
+  return body;
 }
